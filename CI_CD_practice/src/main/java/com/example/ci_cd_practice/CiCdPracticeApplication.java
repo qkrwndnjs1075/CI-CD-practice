3361ef039396a2a6ad982d5e7d1d5b3c8d252f48
박주원 <qkrwndnjs7308@gmail.com>
2025-01-14 16:21:31 +0900

    set

---
+++ CI_CD_practice/src/main/java/com/example/ci_cd_practice/CiCdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPracticeApplication.class, args);
+	}
+
+}
---
+++ CI_CD_practice/src/test/java/com/example/ci_cd_practice/CiCdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
